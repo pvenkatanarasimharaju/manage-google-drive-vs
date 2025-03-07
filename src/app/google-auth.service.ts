a5eb6904ccbab47202395183275914d00d198e4a
venkat <venkata@keka.com>
2025-03-07 23:17:59 +0530

    Added getFiles as seperate function

--- src/app/google-auth.service.ts
+++ src/app/google-auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environment/environment';
+
+import { environment } from '../environment/environment';
 
 @Injectable({
     providedIn: 'root'
