b48c111fea7eb4af6d1c4f316210b19b24962ae5
venkat <venkata@keka.com>
2025-03-07 19:37:19 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoogleAuthService } from './google-auth.service';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
-export class AppComponent {
-  title = 'drive';
+export class AppComponent implements OnInit {
+    accessToken: string | null = null;
+    files: any[] = [];
+    selectedFile: File | null = null;
+    uploadResponse: any = null;
+
+    constructor(private authService: GoogleAuthService) { }
+
+    ngOnInit() {
+        this.accessToken = this.authService.getAccessTokenFromUrl();
+        if (this.accessToken) {
+            this.authService.getDriveFiles(this.accessToken).subscribe((response: any) => {
+                this.files = response.files;
+                console.log(this.files);
+            });
+        }
+    }
+
+    onFileSelected(event: any) {
+        this.selectedFile = event.target.files[0];
+    }
+
+    uploadFile() {
+        if (this.selectedFile && this.accessToken) {
+            this.authService.uploadFile(this.accessToken, this.selectedFile).subscribe(response => {
+                this.uploadResponse = response;
+                console.log('File uploaded:', response);
+            }, error => {
+                console.error('Upload error:', error);
+            });
+        }
+    }
+
+    deleteFile(fileId: string) {
+        if (this.accessToken) {
+            this.authService.deleteFile(this.accessToken, fileId).subscribe(() => {
+                console.log('File deleted:', fileId);
+                this.files = this.files.filter(file => file.id !== fileId);
+            }, (error: any) => {
+                console.error('Error deleting file:', error);
+            });
+        }
+    }
+
+    login() {
+        this.authService.loginWithGoogle();
+    }
+
+    logout() {
+        if (this.accessToken) {
+            this.authService.revokeAccessToken(this.accessToken).subscribe(() => {
+                this.accessToken = null;
+                console.log('Access token revoked');
+                localStorage.removeItem('access_token');
+            });
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { GoogleAuthService } from './google-auth.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    GoogleAuthService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
